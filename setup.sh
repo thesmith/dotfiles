3b80f0ae5e21690592bba186a4e50df3b139117c
Ben Smith <bens@bens-laptop.(none)>
2008-10-12 11:13:30 +0100

    created setup script that simply sets up the dot filesaa

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -sf `pwd`/bashrc ~/.bashrc
+ln -sf `pwd`/gitignore ~/.gitignore
+ln -sf `pwd`/gvimrc ~/.gvimrc
+ln -sf `pwd`/screenrc ~/.screenrc
+ln -sf `pwd`/vim ~/.vim
+ln -sf `pwd`/vimrc ~/.vimrc
