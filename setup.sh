48402ff51ec0413b66bc90d3091a1ecad91d8335
Ben Smith <ben@thesmith.co.uk>
2011-11-03 17:39:24 +0000

    setup ssh config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -sf `pwd`/screenrc ~/.screenrc
 ln -sf `pwd`/vim ~/.vim
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gitconfig ~/.gitconfig
+sudo rm /etc/ssh_config && sudo ln -sf `pwd`/ssh_config /etc/ssh_config
